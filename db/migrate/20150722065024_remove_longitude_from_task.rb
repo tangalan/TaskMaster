ad2b9bc7e62a699dea27eb4e07cdf3eb23cd83f4
tangalan <alan.tangsh@gmail.com>
2015-07-23 04:02:21 -0400

    Trying to get Heroku to work

--- db/migrate/20150722065024_remove_longitude_from_task.rb
+++
@@ -1,5 +0,0 @@
-class RemoveLongitudeFromTask < ActiveRecord::Migration
-  def change
-    remove_column :tasks, :longtitude, :float
-  end
-end
--- db/migrate/20150722065417_remove_latitude_from_task.rb
+++
@@ -1,5 +0,0 @@
-class RemoveLatitudeFromTask < ActiveRecord::Migration
-  def change
-    remove_column :tasks, :latitude, :float
-  end
-end
